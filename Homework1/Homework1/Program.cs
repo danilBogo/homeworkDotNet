f17c5baadb37bd1d10930b20047421546db88507
Danil Bogomolov <danil.bogo@gmail.com>
2021-09-11 18:49:05 +0300

    add all files

---
+++ Homework1/Homework1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework1
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            var check = Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
+            if (check != 0) return check;
+            
+            var result = Calculator.Calculate(val1, operation, val2);
+            Console.WriteLine($"Result is: {result}");
+            return 0;
+        }
+    }
+}
---
+++ Homework1/Tests/Tests.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using Homework1;
+using Xunit;
+
+namespace Tests
+{
+    public class Tests
+    {
+        private const int SuccessfulExecution = 0;
+        private const int WrongArgLength = 1;
+        private const int WrongArgFormatInt = 2;
+        private const int WrongArgFormatOperation = 3;
+
+        [Theory]
+        [InlineData(10, 5)]
+        public void TestCalculatorAddition(int value1, int value2)
+        {
+            Assert.Equal(value1 + value2, Calculator.Calculate(value1, CalculatorOperation.Plus, value2));
+        }
+
+        [Theory]
+        [InlineData(10, 5)]
+        public void TestCalculatorSubtraction(int value1, int value2)
+        {
+            Assert.Equal(value1 - value2, Calculator.Calculate(value1, CalculatorOperation.Minus, value2));
+        }
+
+        [Theory]
+        [InlineData(10, 5)]
+        public void TestCalculatorMultiplication(int value1, int value2)
+        {
+            Assert.Equal(value1 * value2, Calculator.Calculate(value1, CalculatorOperation.Multiply, value2));
+        }
+
+        [Theory]
+        [InlineData(10, 5)]
+        public void TestCalculatorDivision(int value1, int value2)
+        {
+            Assert.Equal(value1 / value2, Calculator.Calculate(value1, CalculatorOperation.Divide, value2));
+        }
+
+        [Theory]
+        [InlineData(new object[] { new string[] { "f", "+", "3" } })]
+        public void TestParserWrongFirstValue(string[] args)
+        {
+            Assert.Equal(WrongArgFormatInt,
+                Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "3", "+", "f" } })]
+        public void TestParserWrongSecondValue(string[] args)
+        {
+            Assert.Equal(WrongArgFormatInt,
+                Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "a", "+", "f" } })]
+        public void TestParserWrongFirstAndSecondValue(string[] args)
+        {
+            Assert.Equal(WrongArgFormatInt,
+                Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "3", ".", "4" } })]
+        public void TestParserWrongOperation(string[] args)
+        {
+            Assert.Equal(WrongArgFormatOperation,
+                Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "3", "+", "4", "5" } })]
+        public void TestParserWrongLength(string[] args)
+        {
+            Assert.Equal(WrongArgLength,
+                Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "f", ".", "4" } })]
+        public void TestParserWrongOperationAndWrongFirstValue(string[] args)
+        {
+            Assert.Equal(WrongArgFormatOperation,
+                Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "4", ".", "f" } })]
+        public void TestParserWrongOperationAndWrongSecondValue(string[] args)
+        {
+            Assert.Equal(WrongArgFormatOperation,
+                Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "4", "+", "3" } })]
+        public void TestParserAdditionCorrectValues(string[] args)
+        {
+            Assert.Equal(SuccessfulExecution,
+                Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "4", "-", "3" } })]
+        public void TestParserSubstractingCorrectValues(string[] args)
+        {
+            Assert.Equal(SuccessfulExecution,
+                Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "4", "*", "3" } })]
+        public void TestParserMultiplicationCorrectValues(string[] args)
+        {
+            Assert.Equal(SuccessfulExecution,
+                Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "4", "/", "3" } })]
+        public void TestParserDivisionCorrectValues(string[] args)
+        {
+            Assert.Equal(SuccessfulExecution,
+                Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "4", "+", "3" } })]
+        public void TestMainAdditionCorrectValues(string[] args)
+        {
+            Assert.Equal(SuccessfulExecution, Program.Main(args));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "4", "-", "3" } })]
+        public void TestMainSubstractingCorrectValues(string[] args)
+        {
+            Assert.Equal(SuccessfulExecution, Program.Main(args));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "4", "*", "3" } })]
+        public void TestMainMultiplicationCorrectValues(string[] args)
+        {
+            Assert.Equal(SuccessfulExecution, Program.Main(args));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "4", "/", "3" } })]
+        public void TestMainDivisionCorrectValues(string[] args)
+        {
+            Assert.Equal(SuccessfulExecution, Program.Main(args));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "f", "+", "3" } })]
+        public void TestMainWrongFirstValue(string[] args)
+        {
+            Assert.Equal(WrongArgFormatInt, Program.Main(args));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "3", "+", "f" } })]
+        public void TestMainWrongSecondValue(string[] args)
+        {
+            Assert.Equal(WrongArgFormatInt, Program.Main(args));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "a", "+", "f" } })]
+        public void TestMainWrongFirstAndSecondValue(string[] args)
+        {
+            Assert.Equal(WrongArgFormatInt, Program.Main(args));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "3", ".", "4" } })]
+        public void TestMainWrongOperation(string[] args)
+        {
+            Assert.Equal(WrongArgFormatOperation, Program.Main(args));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "3", "+", "4", "5" } })]
+        public void TestMainWrongLength(string[] args)
+        {
+            Assert.Equal(WrongArgLength, Program.Main(args));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "f", ".", "4" } })]
+        public void TestMainWrongOperationAndWrongFirstValue(string[] args)
+        {
+            Assert.Equal(WrongArgFormatOperation, Program.Main(args));
+        }
+        
+        [Theory]
+        [InlineData(new object[] { new string[] { "4", ".", "f" } })]
+        public void TestMainWrongOperationAndWrongSecondValue(string[] args)
+        {
+            Assert.Equal(WrongArgFormatOperation, Program.Main(args));
+        }
+    }
+}

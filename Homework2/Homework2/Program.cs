809526eb453067e9eca6c8534af56054e45530d4
Danil Bogomolov <danil.bogo@gmail.com>
2021-09-20 21:11:36 +0300

    add ilproj

--- Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 
 namespace Homework2
 {
@@ -6,12 +7,18 @@ namespace Homework2
     {
         public static int Main(string[] args)
         {
-            var check = Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
-            if (check != 0) return check;
-            
-            var result = Calculator.Calculate(val1, operation, val2);
-            Console.WriteLine($"Result is: {result}");
+            Console.WriteLine(ClassLibrary1.Class.Square(10));
+            //Console.WriteLine("kek");
+            //Square(10);
+            // var check = Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
+            // if (check != 0) return check;
+            //
+            // var result = Calculator.Calculate(val1, operation, val2);
+            // Console.WriteLine($"Result is: {result}");
             return 0;
         }
+        
+        // [MethodImpl(MethodImplOptions.ForwardRef)]
+        // public static extern int Square(int number);
     }
 }

555d902c19f004432d1df4be709adefa8f44823e
Danil Bogomolov <danil.bogo@gmail.com>
2021-09-21 12:35:57 +0300

    add Assembly and update il files and tests

--- Homework2/Homework2/Program.cs
+++ Homework2/Homework2/Program.cs
@@ -1,20 +1,17 @@
-﻿using System;
+﻿using IlClasses;
+using System;
 
 namespace Homework2
 {
-    public class Program
+    public static class Program
     {
         public static int Main(string[] args)
         {
-            //Console.WriteLine(IlClasses.Class.Square(10));
-            //Console.WriteLine(IlClasses.CalculatorOperation.Divide);
-            //Console.WriteLine("kek");
-            //Square(10);
-            // var check = Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
-            // if (check != 0) return check;
-            //
-            // var result = Calculator.Calculate(val1, operation, val2);
-            // Console.WriteLine($"Result is: {result}");
+            var check = Parser.ParseCalcArguments(args, out var val1, out var operation, out var val2);
+            if (check != 0) return check;
+            
+            var result = Calculator.Calculate(val1, operation, val2);
+            Console.WriteLine($"Result is: {result}");
             return 0;
         }
     }

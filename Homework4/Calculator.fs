8f2c0b7bb035c327c3d4ece3dc4abdbd691a0346
Danil Bogomolov <danil.bogo@gmail.com>
2021-10-05 00:17:29 +0300

    remove Homework4

--- Homework4/Calculator.fs
+++ Homework4/Calculator.fs
@@ -1,2 +1,10 @@
 ﻿module Homework4.Calculator
-
+let calculate val1 operation val2 =
+    let val1d = val1 |> double
+    let val2d = val2 |> double
+    
+    match operation with
+    | CalculatorOperation.Plus -> val1d + val2d
+    | CalculatorOperation.Minus -> val1d - val2d
+    | CalculatorOperation.Divide -> val1d / val2d
+    | _ -> val1d  * val2d

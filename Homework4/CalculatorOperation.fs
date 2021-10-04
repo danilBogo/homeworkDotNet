22566610a0453ddeccaa64094ee1cd706d4f7a65
Danil Bogomolov <danil.bogo@gmail.com>
2021-10-05 00:18:36 +0300

    remove Homework4

--- Homework4/CalculatorOperation.fs
+++
@@ -1,7 +0,0 @@
-﻿namespace Homework4
-type CalculatorOperation =
-     | Plus = 1
-     | Minus = 2
-     | Multiply = 3
-     | Divide = 4
-     | UndefinedOperation = 5
--- Homework4/Message.fs
+++
@@ -1,7 +0,0 @@
-﻿namespace Homework4
-type Message =
-    | SuccessfulExecution  = 0
-    | WrongArgLength = 1
-    | WrongArgFormat = 2
-    | WrongArgFormatOperation = 3
-    | DivideByZero = 4

4b049e0d45b2f40df79120c92ede97039fa2587c
Danil Bogomolov <danil.bogo@gmail.com>
2021-10-05 09:22:11 +0300

    hw4 is ready

---
+++ Homework4/Homework4/Calculator.fs
@@ -0,0 +1,10 @@
+﻿module Homework4.Calculator
+let calculate value1 operation value2 =
+    let value1d = value1 |> double
+    let value2d = value2 |> double
+    
+    match operation with
+    | CalculatorOperation.Plus -> value1d + value2d
+    | CalculatorOperation.Minus -> value1d - value2d
+    | CalculatorOperation.Divide -> value1d / value2d
+    | _ -> value1d  * value2d

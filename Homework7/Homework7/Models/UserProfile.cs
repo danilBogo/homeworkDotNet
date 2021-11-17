25b2dbc3761818720b4209a2099d07b2cb8f4794
Danil Bogomolov <danil.bogo@gmail.com>
2021-11-17 19:35:22 +0300

    initial commit

---
+++ Homework7/Homework7/Models/UserProfile.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+#nullable enable
+namespace Homework7
+{
+    public class UserProfile
+    {
+        [Display(Name = "Фамилия")]
+        public string FirstName { get; set; }
+        [Display(Name = "Имя")]
+        public string LastName { get; set; }
+        [Display(Name = "Отчество")]
+        public string? Patronymic { get; set; }
+        public Sex Sex { get; set; }
+        public int Age { get; set; }
+    }
+    
+    public enum Sex
+    {
+        Male,
+        Female
+    };
+}

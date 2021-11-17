24d4fdad6ac649f4b79a55a7ee26e02b121d434f
Danil Bogomolov <danil.bogo@gmail.com>
2021-11-17 19:36:53 +0300

    remove old project

--- Homework6/Homework6/CalculatorOperation.fs
+++
@@ -1,8 +0,0 @@
-﻿namespace Homework6
-
-type CalculatorOperation =
-     | Plus = 0
-     | Minus = 1
-     | Multiply = 2
-     | Divide = 3
-     | UndefinedOperation = 4
--- Homework6/Homework6/MaybeBuilder.fs
+++
@@ -1,9 +0,0 @@
-﻿module Homework6.MaybeBuilder
-
-type MaybeBuilder() =
-    member builder.Bind(a, f) =
-        match a with
-        | Ok resultOk -> f resultOk
-        | Error resultError  -> Error resultError
-    member builder.Return x = Ok x
-let result = MaybeBuilder()

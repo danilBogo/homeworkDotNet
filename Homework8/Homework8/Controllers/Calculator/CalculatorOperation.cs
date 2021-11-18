0cfecdeaca560a145debeb7da2f7cfe822d630f6
Danil Bogomolov <danil.bogo@gmail.com>
2021-11-18 13:01:25 +0300

    add finished project

---
+++ Homework8/Homework8/Controllers/Calculator/CalculatorOperation.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework8.Controllers
+{
+    public enum CalculatorOperation
+    {
+        Plus,
+        Minus,
+        Multiply,
+        Divide
+    }
+}
---
+++ Homework8/Homework8/Controllers/Calculator/ICalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Homework8.Controllers.Calculator
+{
+    public interface ICalculator
+    {
+        string Add(double firstValue, double secondValue);
+        string Subtract(double firstValue, double secondValue);
+        string Multiply(double firstValue, double secondValue);
+        string Divide(double firstValue, double secondValue);
+    }
+}

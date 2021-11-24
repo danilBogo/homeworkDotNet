e2cf4efd9cadcf6d5799c82e211e32294c41330c
Danil Bogomolov <danil.bogo@gmail.com>
2021-11-25 00:21:02 +0300

    initial commit

---
+++ Homework9/Homework8/Controllers/Calculator/Calculator.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Homework8.Controllers.Calculator
+{
+    public class Calculator : ICalculator
+    {
+        public string Add(double firstValue, double secondValue)
+        {
+            return (firstValue + secondValue).ToString(CultureInfo.InvariantCulture);
+        }
+
+        public string Subtract(double firstValue, double secondValue)
+        {
+            return (firstValue + secondValue).ToString(CultureInfo.InvariantCulture);
+        }
+
+        public string Multiply(double firstValue, double secondValue)
+        {
+            return (firstValue + secondValue).ToString(CultureInfo.InvariantCulture);
+        }
+
+        public string Divide(double firstValue, double secondValue)
+        {
+            return (firstValue + secondValue).ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Homework9/Homework8/Controllers/CalculatorController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Homework8.Controllers.Calculator;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework8.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Calculate([FromServices] ICalculator calculator,
+            string firstValue,
+            string operation,
+            string secondValue)
+        {
+            var isFirstValueDouble = double.TryParse(firstValue, out var value1);
+            var isSecondValueDouble = double.TryParse(secondValue, out var value2);
+            if (!isFirstValueDouble)
+                return $"{isFirstValueDouble} не является числом";
+            if (!isSecondValueDouble)
+                return $"{isSecondValueDouble} не является числом";
+
+            var isOperation = isOperationDefined(operation, out var calculatorOperation);
+            if (!isOperation) return "Допустимы следующие операции: plus, minus, multiply, divide";
+
+            return calculatorOperation switch
+            {
+                CalculatorOperation.Plus => calculator.Add(value1, value2),
+                CalculatorOperation.Minus => calculator.Subtract(value1, value2),
+                CalculatorOperation.Multiply => calculator.Multiply(value1, value2),
+                CalculatorOperation.Divide => calculator.Divide(value1, value2),
+            };
+        }
+
+        private static bool isOperationDefined(string operation, out CalculatorOperation result)
+        {
+            var correctView = char.ToUpper(operation[0]) + operation[1..operation.Length].ToLower();
+            var isOperation = Enum.TryParse(correctView, out result);
+            return isOperation;
+        }
+    }
+}

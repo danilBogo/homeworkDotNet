99c09266b05fafa1f8518a4179c5fc291e11afb8
Danil Bogomolov <danil.bogo@gmail.com>
2021-11-28 20:43:00 +0300

    little changes

--- Homework9/Homework8/Startup.cs
+++ Homework9/Homework8/Startup.cs
@@ -35,12 +35,6 @@ namespace Homework8
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();

ececfbbf0aa53a64366308099582a977f1a9dd5e
Danil Bogomolov <danil.bogo@gmail.com>
2021-12-23 13:48:30 +0300

    add methods tests

---
+++ Homework13/Benchmark/Methods.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmark
+{
+    public class Methods
+    {
+        public void Common(string str)
+        {
+            for (var i = 0; i < 1; i++)
+                str += str;
+        }
+
+        public virtual void Virtual(string str)
+        {
+            for (var i = 0; i < 1; i++)
+                str += str;
+        }
+
+        public static void Static(string str)
+        {
+            for (var i = 0; i < 1; i++)
+                str += str;
+        }
+
+        public void Generic<T>(T str)
+        {
+            var currentString = str?.ToString();
+            for (var i = 0; i < 1; i++)
+                currentString += str;
+        }
+
+        public void Dynamic(dynamic str)
+        {
+            for (var i = 0; i < 1; i++)
+                str += str;
+        }
+
+        public void Reflection(string str)
+        {
+            for (var i = 0; i < 1; i++)
+                str += str;
+        }
+    }
+}
---
+++ Homework13/Benchmark/Tests.cs
@@ -0,0 +1,66 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace Benchmark
+{
+    [MinColumn]
+    [MaxColumn]
+    [MedianColumn]
+    [MeanColumn]
+    [StdDevColumn]
+    [MemoryDiagnoser]
+    public class Tests
+    {
+        private const int countTimes = 10000;
+        private const string ConcatenatedString = "aboba";
+
+        private Methods Method { get; set; }
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            Method = new Methods();
+        }
+
+        [Benchmark(Description = "Common method")]
+        public void SimpleMethod()
+        {
+            for (var i = 0; i < countTimes; i++)
+                Method.Common(ConcatenatedString);
+        }
+
+        [Benchmark(Description = "Virtual method")]
+        public void VirtualMethod()
+        {
+            for (var i = 0; i < countTimes; i++)
+                Method.Virtual(ConcatenatedString);
+        }
+
+        [Benchmark(Description = "Generic method")]
+        public void GenericMethod()
+        {
+            for (var i = 0; i < countTimes; i++)
+                Method.Generic(ConcatenatedString);
+        }
+
+        [Benchmark(Description = "Static method")]
+        public void StaticMethod()
+        {
+            for (var i = 0; i < countTimes; i++)
+                Methods.Static(ConcatenatedString);
+        }
+
+        [Benchmark(Description = "Dynamic method")]
+        public void DynamicMethod()
+        {
+            for (var i = 0; i < countTimes; i++)
+                Method.Dynamic(ConcatenatedString);
+        }
+
+        [Benchmark(Description = "Reflection method")]
+        public void ReflectionMethod()
+        {
+            for (var i = 0; i < countTimes; i++)
+                Method.GetType().GetMethod("Reflection")?.Invoke(Method, new object[] {ConcatenatedString});
+        }
+    }
+}
